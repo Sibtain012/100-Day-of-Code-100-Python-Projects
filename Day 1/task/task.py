18b1628b55756c6df0ebd3dac6075495df401e69
sibtain012 <ahmedsabtain949@gmail.com>
2025-03-18 21:02:42 +0500

    100 Days of 100 Python Projects: Day 1, Band Name Generator

---
+++ Day 1/task/task.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator.\n")
+name = input("What's the name of the city you grew up in?\n")
+
+pet_name = input("What is your pet's name?\n")
+
+print("Your brand could be " + name+ " "+ pet_name)
