1b84802b4d7982c71c5389b2ebc7882842b30adb
sibtain012 <ahmedsabtain949@gmail.com>
2025-03-19 22:37:54 +0500

    100 Days of 100 Python Projects: Day2, Tip Calculator

---
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -0,0 +1,10 @@
+# print(len("12345"))
+# print(type(123))
+# print(type(123.33))
+# print(type("123"))
+# print(type(False))
+# print(type(True))
+name = len(input("Enter your name"))
+print("Number of letters in your name: " + str(name))
+
+
